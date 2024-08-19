eb674d0b5e570f7c9c24d498bedfbbfc4aaf4717
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-08-19 13:28:59 +0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
-import App from "./App";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
