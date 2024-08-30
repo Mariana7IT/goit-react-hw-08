0ea8dbcb193c91a6cc60f322f18921d9b580c0c9
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-08-30 20:32:15 +0300

    selectIsLoggedIn

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,11 +1,11 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import { fetchContacts } from "../../redux/contacts/operations";
 import css from "./ContactPage.module.css";
-import { useDispatch } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors.js";
 
 const ContactsPage = () => {
   const dispatch = useDispatch();
